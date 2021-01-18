d325adef902f081ae70ca6f99ef654c5ce1f849b
Shahab2025 <Shahab2025@gmail.com>
2021-01-18 20:03:03 +0330

    Add Models

---
+++ RazorTest1/Model/Users.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorTest1.Model
+{
+    public class Users
+    {
+        public int ID { get; set; }
+        public string  UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string  Mobile { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
